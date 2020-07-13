dc451fd82cb1bb2b356bb0b1ad9f8a30d7a6d5db
Alex Barker <abarke28@gmail.com>
2020-07-12 18:06:00 -0600

    Add CheckNotNull, CheckPositive, CheckNotNegative

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Preconditions
-{
-    public class Class1
-    {
-    }
-}
---
+++ Preconditions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Preconditions
+{
+    public static class Preconditions
+    {
+        public static void CheckNotNull(string input, string parameterName)
+        {
+            if (string.IsNullOrEmpty(input)) throw new ArgumentNullException(parameterName);
+        }
+
+        public static void CheckPositive(int input, string parameterName)
+        {
+            if (input <= 0) throw new ArgumentOutOfRangeException(parameterName);
+        }
+
+        public static void CheckNonNegative(int input, string parameterName)
+        {
+            if (input < 0) throw new ArgumentOutOfRangeException(parameterName);
+        }
+
+
+    }
+}
